01c0cac589a1db0dda37f2e54136a60188b2f0b2
qspang <26760878+shaibearary@users.noreply.github.com>
2023-08-22 22:35:37 +0800

    merit

--- src/routes/merit/+page.svelte
+++ src/routes/merit/+page.svelte
@@ -1,40 +1,76 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import Select from "$lib/components/Select.svelte";
 	import status from '$lib/stores/status';
 	// import PersonCard from '$lib/components/PersonCard.svelte';
 	import type { NDKEvent, NDKZapInvoice } from '@nostr-dev-kit/ndk';
 	import { zapInvoiceFromEvent } from '@nostr-dev-kit/ndk';
 	import ndk from '$lib/stores/ndk';
+	import type{RocketInfo} from '$lib/classes/rocket';
 	import PersonCard from '$lib/components/PersonCard.svelte';
 	import type { AccountInfo } from '$lib/classes/user';
 	// import List from '$lib/classes/list';
 
 	export let data: PageData;
-	// $ndk.fetchEvents({kinds:[1]});
+	let selected: string;
+	let problems
+	Object.values($status.problems).forEach(m => {
+        if (m.ClaimedBy === window.spaceman.pubkey && m.Closed === true) {
+            found++
+            problems.appendChild(makeLinkWithOnclick(m.Title, ()=>{
+                document.getElementById("problem input").value = m.UID
+            }))
+            problems.appendChild(document.createElement("br"))
+        }
+    })
+	let rockets = $status.rockets as Map<string, RocketInfo>;
+	console.log(Object.keys(rockets));
 
-	// Interface created from the any type
-	// Converting the any type to the interface
-	let personData: Record<string, AccountInfo> = data.a.identity as Record<string, AccountInfo>;
+	const rocketIDs: string[] = Object.keys(rockets)
 
-	const sortedAccounts: AccountInfo[] = Object.entries(personData)
-		.sort(([, a], [, b]) => a.Order - b.Order)
-		.map(([, account]) => account);
-	console.log(sortedAccounts, 'hererere');
 </script>
 
-<svelte:head>
-	<title>Listr</title>
-	<meta
-		name="description"
-		content="A Nostr based app to help you view and manage your own
-        Nostr lists and find great content in other people's lists."
-	/>
-</svelte:head>
 
-<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-	<!-- {#key personData} -->
-	{#each sortedAccounts as rocketAccount}
-		<PersonCard {rocketAccount} />
-	{/each}
-	<!-- {/key} -->
+
+<div class="new_mirv">
+	<h3 class="is-3">Let's fkn go!</h3>
+	<div><p>Request Merits</p></div>
+	<div class="field">
+		<label class="label">Rocket ID</label>
+		<Select options={rocketIDs}
+		display_func={o => rockets[o].RocketName}
+		bind:value={selected}/>
+<!-- {JSON.stringify(selected, null, 2)}
+{(console.log(selected),'')} -->
+	</div>
+	<div class="field">
+		<label class="label">Problem ID</label>
+		<div class="control">
+			<div>
+				<input
+					class="input"
+					type="text"
+					placeholder="ID of sovled Problem"
+					id="problem input"
+					maxlength="64"
+				/>
+			</div>
+		</div>
+	</div>
+	<div class="field">
+		<label class="label">Amount</label>
+		<div class="control">
+			<div>
+				<input
+					class="input"
+					type="number"
+					placeholder="Amount in Sats"
+					id="amount input"
+					maxlength="0"
+				/>
+				<p style="display: none;" />
+			</div>
+		</div>
+	</div>
+	<br /><button>Do it!</button>
 </div>
